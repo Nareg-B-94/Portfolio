8c75835c12a104af554193e2e91e34ef75159e24
Nareg Berberian <naregb94@gmail.com>
2022-08-04 01:05:44 +0200

    started with responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FifthSectionComponent } from './fifth-section/fifth-section.component';
 import { FooterComponent } from './footer/footer.component';
 
 
+
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FirstSectionComponent, SecondSectionComponent, ThirdSectionComponent, FourthSectionComponent, FifthSectionComponent, FooterComponent],
   imports: [
